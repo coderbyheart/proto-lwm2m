95f501c441f5f6376be7cb17fdc5c507642668ce
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-11-03 14:22:40 +0100

    test: create typebox def given obj id
    Describe implementation details

---
+++ lwm2m/typeboxDefinition.spec.ts
@@ -0,0 +1,13 @@
+import { describe, it } from 'node:test'
+import assert from 'node:assert'
+import { typeboxDefinition } from 'lwm2m/typeboxDefinition'
+
+void describe('typeboxDefinition', () => {
+	void it(`should create the typebox definition given an ID of a LwM2M object`, () => {
+		const typeDefinition = typeboxDefinition(14201)
+        
+		assert.deepEqual(typeDefinition.properties.Resources.properties[0].title, 'Latitude')
+        assert.deepEqual(typeDefinition.properties.Resources.properties[0].type, 'number')
+        assert.deepEqual(typeDefinition.properties.Resources.properties[0].$id, '0')
+	})
+})
