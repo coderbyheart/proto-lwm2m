b6692118ac1b1574ccbccf0d60dcf73ceed7760e
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-11-05 16:08:30 +0100

    fix: types export was renamed

--- export.js
+++ export.js
@@ -1,4 +1,4 @@
 export * from './dist/senml/SenMLSchema.js'
 export * from './dist/senml/senMLtoLwM2M.js'
 export * from './dist/models/models.js'
-export * from './dist/models/model.js'
+export * from './dist/models/types.js'
