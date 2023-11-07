c55663a8b3b0441406d30fc22a4d175b84a0f56c
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-11-07 16:25:32 +0100

    feat(lwm2m): add enum with Object IDs

--- lwm2m/objects.ts
+++ lwm2m/objects.ts
@@ -5,3 +5,13 @@ export * from './14205.js'
 export * from './14210.js'
 export * from './14220.js'
 export * from './14202.js'
+
+export enum LwM2MObjectID {
+	Geolocation_14201 = 14201,
+	ConnectionInformation_14203 = 14203,
+	DeviceInformation_14204 = 14204,
+	Environment_14205 = 14205,
+	SolarCharge_14210 = 14210,
+	ButtonPress_14220 = 14220,
+	BatteryAndPower_14202 = 14202,
+}
