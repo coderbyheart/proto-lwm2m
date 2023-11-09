1d33fea5d27ad0a9526d7d2f380957183dceaedb
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-11-09 14:58:18 +0100

    feat: transform from LwM2M type to Typebox type
    make transformation for resource definiton

--- lwm2m/createResourceDefinition.spec.ts
+++ lwm2m/createResourceDefinition.spec.ts
@@ -1,6 +1,9 @@
 import { describe, it } from 'node:test'
 import assert from 'node:assert'
-import { createResourceDefinition } from 'lwm2m/createResourceDefinition'
+import {
+	LwM2MType,
+	createResourceDefinition,
+} from 'lwm2m/createResourceDefinition'
 
 void describe('createResourceDefinition', () => {
 	void it(`should create the typebox definition for a resource`, () => {
@@ -9,10 +12,27 @@ void describe('createResourceDefinition', () => {
 			multiple: 'Multiple',
 			mandatory: 'Optional',
 			description: 'The decimal notation of latitude.',
+			type: LwM2MType.String,
 		})
 
 		const expected =
-			"Latitude : Type.Optional(\n        Type.Array(\n        Type.Object({},{description: 'The decimal notation of latitude.',})\n    )\n    )"
+			"Latitude : Type.Optional(\n        Type.Array(\n        Type.String({description: 'The decimal notation of latitude.',})\n    )\n    )"
+
+		assert.equal(resource, expected)
+	})
+
+	void it(`should create typebox def for a resource with DATE type`, () => {
+		const resource = createResourceDefinition({
+			name: 'Timestamp',
+			multiple: 'Single',
+			mandatory: 'Mandatory',
+			description:
+				'The timestamp of when the location measurement was performed.',
+			type: LwM2MType.Time,
+		})
+
+		const expected =
+			"Timestamp : Type.Date({description: 'The timestamp of when the location measurement was performed.',})"
 
 		assert.equal(resource, expected)
 	})
