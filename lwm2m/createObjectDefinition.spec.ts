77fe4d92ee976752ef6785dfba7e555449d60eb7
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-11-09 16:30:58 +0100

    fix: update object definition
    add ObjectID and ObjectVersion as part of typebox definition

--- lwm2m/createObjectDefinition.spec.ts
+++ lwm2m/createObjectDefinition.spec.ts
@@ -5,6 +5,8 @@ import { createObjectDefinition } from 'lwm2m/createObjectDefinition'
 void describe('createObjectDefinition', () => {
 	void it(`should create the typebox definition for an object`, () => {
 		const resource = createObjectDefinition({
+			objectId: '14201',
+			objectVersion: '1.0',
 			multiple: 'Multiple',
 			mandatory: 'Optional',
 			resources: [
@@ -13,7 +15,7 @@ void describe('createObjectDefinition', () => {
 			description: 'Describes the geo location of a device',
 		})
 
-		const expected = `Type.Optional(\n\t\tType.Array(\n\t\tType.Object({Latitude : Type.Optional(\n        Type.Array(\n        Type.Object({},{description: 'The decimal notation of latitude.',})\n    )\n    )}, {description: 'Describes the geo location of a device'})\n\t)\n\t)`
+		const expected = `Type.Optional(\n\t\tType.Array(\n\t\tType.Object({ObjectID: 14201, ObjectVersion: '1.0', Latitude : Type.Optional(\n        Type.Array(\n        Type.Object({},{description: 'The decimal notation of latitude.',})\n    )\n    )}, {description: 'Describes the geo location of a device'})\n\t)\n\t)`
 
 		assert.equal(resource, expected)
 	})
--- lwm2m/createObjectDefinition.ts
+++ lwm2m/createObjectDefinition.ts
@@ -2,17 +2,21 @@
  * Create typebox defintion for object
  */
 export const createObjectDefinition = ({
+	objectId,
+	objectVersion,
 	multiple,
 	mandatory,
 	resources,
 	description,
 }: {
+	objectId: string
+	objectVersion: string
 	multiple: string
 	mandatory: string
 	resources: string[]
 	description: string
 }): string => {
-	let object = `Type.Object({${resources}}, {description: '${description}'})`
+	let object = `Type.Object({ObjectID: ${objectId}, ObjectVersion: '${objectVersion}', ${resources}}, {description: '${description}'})`
 
 	if (multiple === 'Multiple')
 		object = `Type.Array(
