ef9a916bdaa1fbf41eeb0d5e9434910f1f51994e
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-11-10 14:22:24 +0100

    test: check typebox schema as in typebox repo
    see https://github.com/sinclairzx81/typebox/blob/a770b94c737d7771ca249fb645aad8e915032c60/test/runtime/compiler-ajv/validate.ts

--- models/validateDate.spec.ts
+++ models/validateDate.spec.ts
@@ -3,8 +3,81 @@ import assert from 'node:assert'
 import type { Static, TSchema } from '@sinclair/typebox'
 import { Type } from '@sinclair/typebox'
 import type { ErrorObject } from 'ajv'
-import Ajv from 'ajv'
+import Ajv, { type AnySchema } from 'ajv'
+import { TypeGuard } from '@sinclair/typebox'
+import { Value } from '@sinclair/typebox/value'
+import addFormats from 'ajv-formats'
 
+/**
+ * @see https://github.com/sinclairzx81/typebox/blob/a770b94c737d7771ca249fb645aad8e915032c60/test/runtime/compiler-ajv/validate.ts
+ */
+const schemaOf = (schemaOf: string, value: unknown, schema: unknown) => {
+  switch (schemaOf) {
+    case 'Constructor':
+      return TypeGuard.TConstructor(schema) && Value.Check(schema, value)
+    case 'Function':
+      return TypeGuard.TFunction(schema) && Value.Check(schema, value)
+    case 'Date':
+      return TypeGuard.TDate(schema) && Value.Check(schema, value)
+    case 'Promise':
+      return TypeGuard.TPromise(schema) && Value.Check(schema, value)
+    case 'Uint8Array':
+      return TypeGuard.TUint8Array(schema) && Value.Check(schema, value)
+    case 'Undefined':
+      return TypeGuard.TUndefined(schema) && Value.Check(schema, value)
+    case 'Void':
+      return TypeGuard.TVoid(schema) && Value.Check(schema, value)
+    default:
+      return false
+  }
+}
+
+/**
+ * @see https://github.com/sinclairzx81/typebox/blob/a770b94c737d7771ca249fb645aad8e915032c60/test/runtime/compiler-ajv/validate.ts 
+ */
+const createAjv = (references: AnySchema[]) =>  {
+  return addFormats(new Ajv({}), ['date-time', 'time', 'date', 'email', 'hostname', 'ipv4', 'ipv6', 'uri', 'uri-reference', 'uuid', 'uri-template', 'json-pointer', 'relative-json-pointer', 'regex'])
+    .addKeyword({ type: 'object', keyword: 'instanceOf', validate: schemaOf })
+    .addKeyword({ type: 'null', keyword: 'typeOf', validate: schemaOf })
+    .addKeyword('exclusiveMinimumTimestamp')
+    .addKeyword('exclusiveMaximumTimestamp')
+    .addKeyword('minimumTimestamp')
+    .addKeyword('maximumTimestamp')
+    .addKeyword('minByteLength')
+    .addKeyword('maxByteLength')
+    .addSchema(references)
+}
+
+const checkTypeboxOk = <T extends TSchema>(type: T, data: unknown, additional: AnySchema[] = []) => {
+  const ajv = createAjv(additional)
+  function execute() {
+    // required as ajv will throw if referenced schema is not found
+    try {
+      return ajv.validate(type, data) as boolean
+    } catch {
+      return false
+    }
+  }
+  console.log('---------------------------')
+  console.log('type')
+  console.log('---------------------------')
+  console.log(JSON.stringify(type, null, 2))
+  console.log('---------------------------\n\n')
+  console.log('data')
+  console.log('---------------------------')
+  console.log(JSON.stringify(data, null, 2), typeof data)
+  console.log('---------------------------\n\n')
+  if (execute() === false) {
+    console.log('errors')
+    console.log('---------------------------')
+    console.log(ajv.errorsText(ajv.errors))
+    throw Error('validation fails')
+  }
+}
+
+/**
+ * Typebox type definition
+ */
 export const test_99 = Type.Object({
 	99: Type.Date({
 		$id: '99',
@@ -18,7 +91,7 @@ export const test_99 = Type.Object({
 /**
  * @see node_modules/@hello.nrfcloud.com/proto/validator/validateWithTypeBox.ts
  *
- * ajv compilation check is not longer supported in Typebox
+ * seems like ajv compilation check is not longer supported in Typebox
  * @see https://github.com/sinclairzx81/typebox/blob/master/test/runtime/compiler-ajv/date.ts
  */
 export const validateWithTypeBox = <T extends TSchema>(
@@ -55,10 +128,21 @@ void describe('validateDate', () => {
 
 		assert.deepEqual(new Date(1698155694), object[99])
 
+    const T1 = Type.String()
+    checkTypeboxOk(T1, '1')
+
+    checkTypeboxOk(test_99, object)
+
+
+    const T = Type.Date()
+    checkTypeboxOk(T, new Date())
+    
+    /*
 		const result = validateWithTypeBox(test_99)(object) as unknown as {
 			value: unknown
 		}
 
 		assert.deepEqual(result.value, object)
+    */
 	})
 })
