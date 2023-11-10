615717a41b0ef64a3f9f2a533c53d014f2d726c9
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-11-10 16:27:08 +0100

    test: validate lwm2m obj follow typebox def
    use validateWithTypeBox function from './validateWithTypebox.js' to validate dates

--- models/validateLwM2M.spec.ts
+++ models/validateLwM2M.spec.ts
@@ -1,8 +1,8 @@
 import { describe, it } from 'node:test'
 import assert from 'node:assert'
-import { validateWithTypeBox } from '@hello.nrfcloud.com/proto'
 import { Geolocation_14201 } from '../lwm2m/14201.js'
 import type { Static } from '@sinclair/typebox'
+import { validateWithTypeBox } from './validateWithTypebox.js'
 
 void describe('validateLwM2M', () => {
 	void it(`should validate LwM2M object follows type  definition`, async () => {
@@ -17,7 +17,7 @@ void describe('validateLwM2M', () => {
 				4: 26.376304626464844,
 				5: 359.1545715332,
 				6: 'GNSS',
-				99: new Date(1698155694), // TODO validation fail in ajv.compile(schema). Fix it
+				99: new Date(1698155694),
 			},
 		}
 
