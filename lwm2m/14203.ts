e3930470e601205f623e84d68f9776e841a36096
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-11-14 16:57:12 +0100

    fix: update imports

--- lwm2m/14203.ts
+++ lwm2m/14203.ts
@@ -1,5 +1,4 @@
-import type { LwM2MObjectID } from './LwM2MObjectID'
-import type { LwM2MObject } from './objects'
+import type { LwM2MObject, LwM2MObjectID } from './objects'
 
 /**
  * Connection information (14203)
