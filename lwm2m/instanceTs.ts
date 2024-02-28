57709e4f9d770d6497d253afa784c0e1f03c5f7d
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-02-28 10:50:19 +0100

    feat(lwm2m): add instanceTs()

---
+++ lwm2m/instanceTs.ts
@@ -0,0 +1,10 @@
+import type { LwM2MObjectInstance } from '../senml/senMLtoLwM2M.js'
+import { definitions } from './definitions.js'
+import { timestampResources } from './timestampResources.js'
+
+export const instanceTs = (instance: LwM2MObjectInstance): Date => {
+	const definition = definitions[instance.ObjectID]
+	const tsResourceId = timestampResources[definition.ObjectID] as number // All registered objects must have a timestamp resource
+	const ts = instance.Resources[tsResourceId] as string
+	return new Date(ts)
+}
