5dea9e79482306808fe7030b98305fe0f48648a2
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-04-15 14:15:21 +0200

    feat(context): add generic context

--- api/Context.ts
+++ api/Context.ts
@@ -7,4 +7,5 @@ export const Context = {
 		request: new URL(`${baseURL}/share-device-request`),
 		ownershipConfirmed: new URL(`${baseURL}/share-device-ownership-confirmed`),
 	},
+	named: (name: string): URL => new URL(`${baseURL}/${name}`),
 }
