dbedd3c01617dffc4796e12c05bbfdeb77bd0804
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-04-18 12:26:30 +0200

    fix(api): share device request has deviceId property

--- api/ShareDeviceRequest.ts
+++ api/ShareDeviceRequest.ts
@@ -1,8 +1,9 @@
 import { Type } from '@sinclair/typebox'
 import { Context } from './Context.js'
-import { PublicDeviceId } from './DeviceId.js'
+import { DeviceId, PublicDeviceId } from './DeviceId.js'
 
 export const ShareDeviceRequest = Type.Object({
 	'@context': Type.Literal(Context.shareDevice.request.toString()),
 	id: PublicDeviceId,
+	deviceId: DeviceId,
 })
