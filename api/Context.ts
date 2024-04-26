d4e95b8a2ab37c126fdeb12eba683d30d8dfcf9f
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-04-26 15:36:24 +0200

    feat: history and updates are provided by the backend
    BREAKING CHANGE: this removes History and ObjectUpdate

--- api/Context.ts
+++ api/Context.ts
@@ -3,13 +3,9 @@ const baseURL = `https://github.com/hello-nrfcloud/proto-map`
 export const Context = {
 	device: new URL(`${baseURL}/device`),
 	devices: new URL(`${baseURL}/devices`),
-	history: {
-		resource: new URL(`${baseURL}/history/resource`),
-	},
 	shareDevice: {
 		request: new URL(`${baseURL}/share-device-request`),
 		ownershipConfirmed: new URL(`${baseURL}/share-device-ownership-confirmed`),
 	},
-	objectUpdate: new URL(`${baseURL}/object/update`),
 	named: (name: string): URL => new URL(`${baseURL}/${name}`),
 }
