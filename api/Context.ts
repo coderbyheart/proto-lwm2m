3f03ada62b80babb2d6de9bb11a35491907e5967
Markus Tacker <m@coderbyheart.com>
2024-07-18 09:05:26 +0200

    feat(api): add DeviceJWT

--- api/Context.ts
+++ api/Context.ts
@@ -3,9 +3,11 @@ const baseURL = `https://github.com/hello-nrfcloud/proto-map`
 export const Context = {
 	device: new URL(`${baseURL}/device`),
 	devices: new URL(`${baseURL}/devices`),
+	deviceJWT: new URL(`${baseURL}/device-jwt`),
 	shareDevice: {
 		request: new URL(`${baseURL}/share-device-request`),
 		ownershipConfirmed: new URL(`${baseURL}/share-device-ownership-confirmed`),
 	},
 	named: (name: string): URL => new URL(`${baseURL}/${name}`),
+	apiHealth: new URL(`${baseURL}/api/health`),
 }
