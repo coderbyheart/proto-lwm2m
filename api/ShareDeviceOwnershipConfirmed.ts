c1c613eb7f382db8f5816d2388b4c066ac89df25
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-08-06 15:10:37 +0200

    fix: remove sharing device messages
    Sharing workflow is now done without these.
    
    BREAKING CHANGE: sharing messages are removed

--- api/ShareDeviceOwnershipConfirmed.ts
+++
@@ -1,8 +0,0 @@
-import { Type } from '@sinclair/typebox'
-import { Context } from './Context.js'
-import { DeviceId } from './DeviceId.js'
-
-export const ShareDeviceOwnershipConfirmed = Type.Object({
-	'@context': Type.Literal(Context.shareDevice.ownershipConfirmed.toString()),
-	id: DeviceId,
-})
--- api/ShareDeviceRequest.ts
+++
@@ -1,9 +0,0 @@
-import { Type } from '@sinclair/typebox'
-import { Context } from './Context.js'
-import { DeviceId, PublicDeviceId } from './DeviceId.js'
-
-export const ShareDeviceRequest = Type.Object({
-	'@context': Type.Literal(Context.shareDevice.request.toString()),
-	id: PublicDeviceId,
-	deviceId: DeviceId,
-})
